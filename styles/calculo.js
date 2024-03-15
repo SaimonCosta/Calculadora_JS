ee5c2d8e660998a60ab8d4d4c05a9c9472f28d8a
SaimonCosta <saimonsantoscosta@outlook.com>
2024-03-15 18:03:51 -0300

    animações e mudança layout + functions JS

--- styles/calculo.js
+++ styles/calculo.js
@@ -1,15 +1,38 @@
-let numerosAdicionados = [];
+// let numerosAdicionados = [];
 
-localStorage.s
+// localStorage.s
 
-function insert(value) {
-    numerosAdicionados.push(value);
+// function insert(value) {
+//     numerosAdicionados.push(value);
 
-    document.querySelector(".texto_resultado").innerHTML = numerosAdicionados.join("");
+//     document.querySelector(".texto_resultado").innerHTML = numerosAdicionados.join("");
+// }
+
+// function vezes() {
+//     localStorage.setItem(numerosAdicionados.length, numerosAdicionados.join(""))
+
+//     document.querySelector(".texto_resultado").innerHTML = "";
+// }
+
+
+
+function adicionar(valor) {
+   
+    document.querySelector('.texto_resultado').innerText += valor;
 }
 
-function vezes() {
-    localStorage.setItem(numerosAdicionados.length, numerosAdicionados.join(""))
 
-    document.querySelector(".texto_resultado").innerHTML = "";
+function limpar() {
+
+    document.querySelector('.texto_resultado').innerText = '';
+}
+
+
+function calcular() {
+   
+    let expressao = document.querySelector('.texto_resultado').innerText;
+  
+    let resultado = eval(expressao); 
+    
+    document.querySelector('.texto_resultado').innerText = resultado;
 }
